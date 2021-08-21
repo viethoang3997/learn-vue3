6eb20e2acc3fc04cbf8244b334a51569ac8b78b9
hoangtv3997 <hoang392210@gmail.com>
2021-08-22 00:18:08 +0700

    learn Props dynamic

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <p :class="['todo-item', todoProps.completed ? 'is-completed' : '']">
+    <input type="checkbox" :checked="todoProps.completed"/>
+    {{ todoProps.title }}
+    <button class="del-btn">Delete</button>
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['todoProps']
+}
+</script>
+
+<style scope>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  margin: 0;
+  border-bottom: 1px #ccc dotted;
+}
+
+.is-completed {
+  text-decoration: line-through;
+}
+
+.del-btn {
+  background-color: crimson;
+  color: white;
+  border: none;
+  cursor: pointer;
+  float: right;
+}
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,38 @@
 <template>
-  <p>Việc 1</p>
-  <p>Việc 2</p>
-  <p>Việc 3</p>
+  <TodoItem v-for="todo in todos" v-bind:key="todo.id" v-bind:todoProps="todo">{{ todo }}</TodoItem>
+  
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue'
 export default {
-  name: 'Todos'
+  name: 'Todos',
+  components: {
+    TodoItem,
+  },
+ 
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Việc 1',
+          completed: true
+        },
+        {
+          id: 2,
+          title: 'Việc 2',
+          completed: false
+        }, 
+        {
+          id: 3,
+          title: 'Việc 3',
+          completed: false
+        }  
+      ],
+    }
+  }
+  
 }
 </script>
 
